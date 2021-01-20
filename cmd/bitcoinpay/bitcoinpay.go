ca53ef0b0c2c78ded2cff91ec1baa2a03613a874
Singdeer <singdeerlet@gmail.com>
2021-01-20 22:31:51 +0800

    BUG:code missing by git

---
+++ cmd/bitcoinpay/bitcoinpay.go
@@ -0,0 +1,144 @@
+// Copyright (c) 2020-2021 The bitcoinpay developers
+// Copyright (c) 2015-2016 The Decred developers
+// Copyright (c) 2013-2016 The btcsuite developers
+
+package main
+
+import (
+	"fmt"
+	"github.com/btceasypay/bitcoinpay/core/message"
+	_ "github.com/btceasypay/bitcoinpay/database/ffldb"
+	"github.com/btceasypay/bitcoinpay/log"
+	"github.com/btceasypay/bitcoinpay/node"
+	"github.com/btceasypay/bitcoinpay/params"
+	"github.com/btceasypay/bitcoinpay/services/common"
+	"github.com/btceasypay/bitcoinpay/services/index"
+	"github.com/btceasypay/bitcoinpay/version"
+	"os"
+	"runtime"
+	"runtime/debug"
+)
+
+func main() {
+	// Initialize the goroutine count,  Use all processor cores.
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// Block and transaction processing can cause bursty allocations.  This
+	// limits the garbage collector from excessively overallocating during
+	// bursts.  This value was arrived at with the help of profiling live
+	// usage.
+	debug.SetGCPercent(20)
+
+	// Work around defer not working after os.Exit()
+	if err := bitcoinpayMain(nil); err != nil {
+		os.Exit(1)
+	}
+}
+
+// bitcoinpayMain is the real main function for bitcoinpay.  It is necessary to work around
+// the fact that deferred functions do not run when os.Exit() is called.  The
+// optional nodeChan parameter is mainly used by the service code to be
+// notified with the node once it is setup so it can gracefully stop it when
+// requested from the service control manager.
+func bitcoinpayMain(nodeChan chan<- *node.Node) error {
+	// Load configuration and parse command line.  This function also
+	// initializes logging and configures it accordingly.
+	cfg, _, err := common.LoadConfig()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if common.LogWrite() != nil {
+			common.LogWrite().Close()
+		}
+	}()
+	// Get a channel that will be closed when a shutdown signal has been
+	// triggered either from an OS signal such as SIGINT (Ctrl+C) or from
+	// another subsystem such as the RPC server.
+	interrupt := interruptListener()
+	defer log.Info("Shutdown complete")
+
+	// Show version and home dir at startup.
+	log.Info("System info", "Bitcoinpay Version", version.String(), "Go version", runtime.Version())
+	log.Info("System info", "UUID", message.UUID)
+	log.Info("System info", "Home dir", cfg.HomeDir)
+
+	if cfg.NoFileLogging {
+		log.Info("File logging disabled")
+	}
+
+	// Load the block database.
+	db, err := common.LoadBlockDB(cfg)
+	if err != nil {
+		log.Error("load block database", "error", err)
+		return err
+	}
+	defer func() {
+		// Ensure the database is sync'd and closed on shutdown.
+		log.Info("Gracefully shutting down the database...")
+		db.Close()
+	}()
+
+	// Return now if an interrupt signal was triggered.
+	if interruptRequested(interrupt) {
+		return nil
+	}
+	// Drop indexes and exit if requested.
+	if cfg.DropAddrIndex {
+		if err := index.DropAddrIndex(db, interrupt); err != nil {
+			log.Error("%v", err)
+			return err
+		}
+
+		return nil
+	}
+	if cfg.DropTxIndex {
+		if err := index.DropTxIndex(db, interrupt); err != nil {
+			log.Error(fmt.Sprintf("%v", err))
+			return err
+		}
+
+		return nil
+	}
+
+	// Cleanup the block database
+	if cfg.Cleanup {
+		db.Close()
+		common.CleanupBlockDB(cfg)
+		return nil
+	}
+
+	// Create node and start it.
+	n, err := node.NewNode(cfg, db, params.ActiveNetParams.Params, shutdownRequestChannel)
+	if err != nil {
+		log.Error("Unable to start server", "listeners", cfg.Listeners, "error", err)
+		return err
+	}
+	err = n.RegisterService()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		log.Info("Gracefully shutting down the server...")
+		err := n.Stop()
+		if err != nil {
+			log.Warn("node stop error", "error", err)
+		}
+		n.WaitForShutdown()
+	}()
+	err = n.Start()
+	if err != nil {
+		log.Error("Uable to start server", "error", err)
+		return err
+	}
+
+	if nodeChan != nil {
+		nodeChan <- n
+	}
+	// Wait until the interrupt signal is received from an OS signal or
+	// shutdown is requested through one of the subsystems such as the RPC
+	// server.
+	<-interrupt
+	return nil
+}
